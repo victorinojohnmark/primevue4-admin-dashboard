319e2ef10732a849a5393836369c703c0303295a
victorinojohnmark <victorinojohnmark@gmail.com>
2024-09-04 22:10:23 +0800

    Initial setup

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import './assets/style.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+import App from './App.vue'
+import router from './router'
+
+// PrimeVue Components
+import Button from "primevue/button"
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            cssLayer: {
+                name: 'primevue',
+                order: 'tailwind-base, primevue, tailwind-utilities'
+            }
+        }
+    }
+});
+
+app.component('Button', Button);
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <h1>Home</h1>
+    <Button label="Submit" size="small" icon="pi pi-user" text />
+  </main>
+</template>
+
+<script setup>
+</script>
